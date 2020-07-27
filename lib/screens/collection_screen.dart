ef2fe953d1acb9b41413237f03ba122108d89874
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-07-27 15:40:54 +0200

    Add features

---
+++ lib/screens/collection_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:cdli_tablet_app/models/collection_model.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+class CollectionScreen extends StatefulWidget {
+  @override
+  _CollectionScreenState createState() => _CollectionScreenState();
+}
+
+class _CollectionScreenState extends State<CollectionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        iconTheme: new IconThemeData(color: Colors.white),
+        title: Text(
+          'cdli tablet',
+          style: TextStyle(color: Colors.white, fontFamily: 'NotoSansJP',
+            fontWeight: FontWeight.w400,),
+        ),
+        backgroundColor: Colors.black,
+        leading: PlatformIconButton(
+          android: (_) => MaterialIconButtonData(icon: Icon(Icons.arrow_back, color: Colors.white,)),
+          ios: (_) => CupertinoIconButtonData(icon: Icon(Icons.arrow_back_ios, color: Colors.white,)),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: CollectionModel(),
+    );
+  }
+}
