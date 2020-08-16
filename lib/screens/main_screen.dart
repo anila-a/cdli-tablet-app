f5e51659ca74a075c56fd3d23d9571429a247395
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-08-16 20:35:30 +0200

    Update main_screen.dart

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,194 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:cdli_tablet_app/models/main_model.dart';
 import 'package:cdli_tablet_app/screens/search_screen.dart';
-import 'package:cdli_tablet_app/screens/about_screen.dart';
-import 'package:cdli_tablet_app/screens/help_screen.dart';
-import 'package:cdli_tablet_app/screens/grid_screen.dart';
-import 'package:cdli_tablet_app/screens/list_screen.dart';
+import 'package:cdli_tablet_app/routes/menu.dart';
 import 'package:flutter/services.dart';
 
-class MainScreen extends StatefulWidget {
-  @override
-  _MainScreenState createState() => _MainScreenState();
-}
+class MainScreen extends StatelessWidget with NavigationState {
 
-class _MainScreenState extends State<MainScreen> {
+  final Function onMenuTap;
+
+  const MainScreen({Key key, this.onMenuTap}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: _onBackPressed,
+      onWillPop: () {
+        _onBackPressed(context);
+      },
       child: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          iconTheme: new IconThemeData(color: Colors.white),
-          title: Text(
-            'cdli tablet',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'NotoSansJP',
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          backgroundColor: Color.fromRGBO(18, 18, 18, 1),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.search,
-                color: Colors.white,
+            backgroundColor: Colors.black,
+            appBar: AppBar(
+              iconTheme: IconThemeData(color: Colors.white),
+              title: Text(
+                'cdli tablet',
+                style: TextStyle(color: Colors.white, fontFamily: 'NotoSansJP',
+                  fontWeight: FontWeight.w400,),
               ),
-              tooltip: 'Search',
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SearchScreen()));
-              },
-            ),
-          ],
-        ),
-        drawer: ClipRRect(
-          borderRadius: BorderRadius.only(
-              topRight: Radius.circular(16.0),
-              bottomRight: Radius.circular(16.0)),
-          child: Drawer(
-            child: Container(
-              color: Color.fromRGBO(18, 18, 18, 1),
-              child: ListView(
-                children: <Widget>[
-                  DrawerHeader(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.fitWidth,
-                        image: AssetImage('assets/images/ic_launcher.png'),
-                      ),
-                    ),
-                    child: Container(
-                      alignment: Alignment.centerLeft,
-                      child: Column(children: <Widget>[
-                        SizedBox(
-                          height: 25,
-                          //height: 45,
-                        ),
-                        Image.asset(
-                          'assets/images/ic_cdlilogo.png',
-                          //Image.asset('assets/images/logo-no-text.jpg',
-                          scale: 8.5,
-                          alignment: Alignment.centerLeft,
-                        ),
-                      ]),
-                    ),
-                  ),
-                  //Divider(color: Colors.white,),
-                  ListTile(
-                    //leading: Icon(Icons.grid_on, color: Colors.white, size: 22,),
-                    title: Text(
-                      'APP',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 17,
-                        fontFamily: 'NotoSansJP',
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
-                  ListTile(
-                      leading: Icon(
-                        Icons.grid_on,
-                        color: Colors.white,
-                        size: 22,
-                      ),
-                      title: Text(
-                        'Grid Layout',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 17,
-                          fontFamily: 'NotoSansJP',
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => GridScreen()),
-                        );
-                      }),
-                  ListTile(
-                      leading: Icon(
-                        Icons.sort_by_alpha,
-                        color: Colors.white,
-                        size: 22,
-                      ),
-                      title: Text(
-                        'List Layout',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 17,
-                          fontFamily: 'NotoSansJP',
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => ListScreen()),
-                        );
-                      }),
-                  Divider(
+              backgroundColor: Colors.black,
+              leading: InkWell(
+                  child: Icon(Icons.menu, color: Colors.white),
+                  onTap: onMenuTap,
+                ),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.search,
                     color: Colors.white,
-                    thickness: 0.28,
                   ),
-                  ListTile(
-                      leading: Icon(
-                        Icons.info_outline,
-                        color: Colors.white,
-                        size: 22,
-                      ),
-                      title: Text(
-                        'About',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 17,
-                          fontFamily: 'NotoSansJP',
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => AboutScreen()),
-                        );
-                      }),
-                  ListTile(
-                      leading: Icon(
-                        Icons.help_outline,
-                        color: Colors.white,
-                        size: 22,
-                      ),
-                      title: Text(
-                        'Help and Feedback',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 17,
-                          fontFamily: 'NotoSansJP',
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => HelpScreen()),
-                        );
-                      }),
-                ],
+                  tooltip: 'Search',
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SearchScreen()));
+                  },
+                ),
+              ],
               ),
-            ),
+            body: MainModel(),
           ),
-        ),
-        body: MainModel(),
-      ),
     );
   }
 
-  Future<bool> _onBackPressed() {
+  Future<bool> _onBackPressed(BuildContext context) {
     return showDialog<void>(
       context: context,
       barrierDismissible: false, // user must tap button!
