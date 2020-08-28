5985e279b0c0b1eb783c849438a476ca67873221
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-08-28 16:09:58 +0200

    Set gridDelegation as null

--- lib/models/grid_model.dart
+++ lib/models/grid_model.dart
@@ -85,9 +85,9 @@ class _GridModelState extends State<GridModel> {
             Expanded(
               flex: 1,
             child: GridView.builder(
-                /* gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount:
-                (orientation == Orientation.portrait) ? 2 : 3), */
+                gridDelegate: null,
+                /*SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount:
+                (orientation == Orientation.portrait) ? 2 : 3),*/
                 itemCount: dataState.list.length,
                 itemBuilder: (BuildContext context, int index) {
                   int position = index;
